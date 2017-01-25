6dccde28b2c0f45ef4b1878ddbd32d063b244142
Mathis <ryb.mathis@hotmail.fr>
2017-01-25 22:48:38 +0100

    Différents boutons créés pour l'administrateur Il faut maintenant faire d'autres interfaces relatives aux btn Et leur fonction

--- Agenda/GestionAgenda/src/fr/villalem/admin/ITadmin.java
+++ Agenda/GestionAgenda/src/fr/villalem/admin/ITadmin.java
@@ -29,11 +29,21 @@ public class ITadmin extends javax.swing.JFrame {
 
         jLabel1 = new javax.swing.JLabel();
         btnQuitter = new javax.swing.JButton();
+        btnAjoutSalle = new javax.swing.JButton();
+        btnModificationSalle = new javax.swing.JButton();
+        btnSupprimerSalle = new javax.swing.JButton();
+        btnCreationUtilisateur = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("Tu es sur l'interface administrateur lol");
+        jLabel1.setText("Paramètres administrateur");
 
+        btnQuitter.setBackground(java.awt.Color.green);
         btnQuitter.setText("Quitter");
         btnQuitter.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -41,27 +51,82 @@ public class ITadmin extends javax.swing.JFrame {
             }
         });
 
+        btnAjoutSalle.setText("Ajouter une salle");
+
+        btnModificationSalle.setText("Modifier une salle");
+
+        btnSupprimerSalle.setText("Supprimer une salle");
+
+        btnCreationUtilisateur.setText("Créer un nouvel utilisateur");
+
+        jButton1.setText("Modifier un utilisateur");
+
+        jButton2.setText("Supprimer un utilisateur");
+
+        jButton3.setText("Accéder à l'historique");
+
+        jButton4.setText("Consulter une facture");
+
+        jButton5.setText("Voir les statistiques");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(100, Short.MAX_VALUE)
-                .addComponent(jLabel1)
-                .addGap(60, 60, 60))
             .addGroup(layout.createSequentialGroup()
-                .addGap(151, 151, 151)
-                .addComponent(btnQuitter)
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
+                    .addComponent(jLabel1)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnAjoutSalle)
+                        .addGap(121, 121, 121)
+                        .addComponent(btnModificationSalle)
+                        .addGap(134, 134, 134)
+                        .addComponent(btnSupprimerSalle))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnCreationUtilisateur)
+                        .addGap(121, 121, 121)
+                        .addComponent(jButton1)
+                        .addGap(134, 134, 134)
+                        .addComponent(jButton2))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButton3)
+                        .addGap(121, 121, 121)
+                        .addComponent(jButton4)
+                        .addGap(134, 134, 134)
+                        .addComponent(jButton5)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(6, 6, 6)
+                .addComponent(btnQuitter, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(6, 6, 6))
         );
+
+        layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {btnAjoutSalle, btnCreationUtilisateur, btnModificationSalle, btnSupprimerSalle, jButton1, jButton2, jButton3, jButton4, jButton5});
+
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(116, 116, 116)
+                .addContainerGap()
                 .addComponent(jLabel1)
+                .addGap(19, 19, 19)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnAjoutSalle)
+                    .addComponent(btnModificationSalle)
+                    .addComponent(btnSupprimerSalle))
                 .addGap(18, 18, 18)
-                .addComponent(btnQuitter)
-                .addContainerGap(121, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnCreationUtilisateur)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton3)
+                    .addComponent(jButton4)
+                    .addComponent(jButton5))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 58, Short.MAX_VALUE)
+                .addComponent(btnQuitter, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(37, 37, 37))
         );
 
         pack();
@@ -73,7 +138,16 @@ public class ITadmin extends javax.swing.JFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAjoutSalle;
+    private javax.swing.JButton btnCreationUtilisateur;
+    private javax.swing.JButton btnModificationSalle;
     private javax.swing.JButton btnQuitter;
+    private javax.swing.JButton btnSupprimerSalle;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
