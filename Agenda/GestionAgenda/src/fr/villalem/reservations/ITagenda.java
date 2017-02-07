4d519f0fc8afc834c894ccf9e798488208a6c857
Alexis <adoualle@gmail.com>
2017-02-07 15:08:15 +0100

    test graphique

--- Agenda/GestionAgenda/src/fr/villalem/reservations/ITagenda.java
+++ Agenda/GestionAgenda/src/fr/villalem/reservations/ITagenda.java
@@ -223,7 +223,8 @@ public class ITagenda extends javax.swing.JFrame {
     }//GEN-LAST:event_jMenuChercherConflitActionPerformed
 
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
-        // TODO add your handling code here:
+        testAgenda t = new testAgenda();
+        t.setVisible(true);
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     //Méthodes
