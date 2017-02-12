1c36938f49d0580d315c886275da69109b986d9f
Mathis <ryb.mathis@hotmail.fr>
2017-02-12 15:16:37 +0100

    Finir création facture

--- Agenda/GestionAgenda/src/fr/villalem/factures/Factures.java
+++ Agenda/GestionAgenda/src/fr/villalem/factures/Factures.java
@@ -30,7 +30,9 @@ public class Factures {
     
     public static void creerFacture(){
     
-        //Création des variables
+        /*
+        Création des variables
+        */
         HSSFWorkbook wb = new HSSFWorkbook();
         HSSFSheet sheet = wb.createSheet("Format");
         HSSFCellStyle cellStyle = null;
@@ -38,7 +40,81 @@ public class Factures {
         HSSFCell cell = null;
         HSSFFont fonte = null;
         
-        //Redimensionnement des lignes et colonnes par défaut
+        /*
+        DEFINITION DES CELL STYLES
+        */
+        //Bordure droite
+        HSSFCellStyle cellStyleRight = wb.createCellStyle();
+        cellStyleRight.setBorderRight(BorderStyle.MEDIUM);
+        cellStyleRight.setRightBorderColor((short)8);
+        //Bordure gauche
+        HSSFCellStyle cellStyleLeft = wb.createCellStyle();
+        cellStyleLeft.setBorderLeft(BorderStyle.MEDIUM);
+        cellStyleLeft.setLeftBorderColor((short)8);
+        //Bordure haut
+        HSSFCellStyle cellStyleTop = wb.createCellStyle();
+        cellStyleTop.setBorderTop(BorderStyle.MEDIUM);
+        cellStyleTop.setTopBorderColor((short)8);
+        //Bordure bas
+        HSSFCellStyle cellStyleBottom = wb.createCellStyle();
+        cellStyleBottom.setBorderBottom(BorderStyle.MEDIUM);
+        cellStyleBottom.setBottomBorderColor((short)8);
+        
+        
+        /*
+        DEFINITION DES FONTS
+        */
+        //Font 12
+        HSSFFont fonte12 = wb.createFont();
+        fonte12.setFontHeightInPoints((short)12);
+        fonte12.setFontName("Courier New");
+        HSSFCellStyle cellStyleFont12 = wb.createCellStyle();
+        cellStyleFont12.setFont(fonte12);
+        //Font 16
+        HSSFFont fonte16 = wb.createFont();
+        fonte16.setFontHeightInPoints((short)16);
+        fonte16.setFontName("Courier New");
+        HSSFCellStyle cellStyleFont16 = wb.createCellStyle();
+        cellStyleFont16.setFont(fonte16);
+        //Font 16 gras
+        HSSFFont fonte16Gras = wb.createFont();
+        fonte16Gras.setFontHeightInPoints((short)16);
+        fonte16Gras.setFontName("Courier New");
+        fonte16Gras.setBold(true);
+        HSSFCellStyle cellStyleFont16Gras = wb.createCellStyle();
+        cellStyleFont16Gras.setFont(fonte16Gras);
+        //Font 18
+        HSSFFont fonte18 = wb.createFont();
+        fonte18.setFontHeightInPoints((short)18);
+        fonte18.setFontName("Courier New");
+        HSSFCellStyle cellStyleFont18 = wb.createCellStyle();
+        cellStyleFont18.setFont(fonte18);
+        //Font 18 gras
+        HSSFFont fonte18Gras = wb.createFont();
+        fonte18Gras.setFontHeightInPoints((short)18);
+        fonte18Gras.setFontName("Courier New");
+        fonte18Gras.setBold(true);
+        HSSFCellStyle cellStyleFont18Gras = wb.createCellStyle();
+        cellStyleFont18Gras.setFont(fonte18Gras);
+        
+        /*
+        Fusion des cellules
+        */
+        sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, 2)); // Fusionne les cellules de la ligne 0 à la ligne 0 et colonnes 0 à colonne 2
+        sheet.addMergedRegion(new CellRangeAddress(1,1,0,3));
+        sheet.addMergedRegion(new CellRangeAddress(2, 2, 0, 2));
+        sheet.addMergedRegion(new CellRangeAddress(3, 3, 0, 3));
+        sheet.addMergedRegion(new CellRangeAddress(4, 4, 0, 1));
+        sheet.addMergedRegion(new CellRangeAddress(6, 6, 0, 1));
+        sheet.addMergedRegion(new CellRangeAddress(7, 7, 4, 5));
+        sheet.addMergedRegion(new CellRangeAddress(8, 8, 4, 5));
+        sheet.addMergedRegion(new CellRangeAddress(9, 9, 4, 5));
+        sheet.addMergedRegion(new CellRangeAddress(10, 10, 4, 5));
+        sheet.addMergedRegion(new CellRangeAddress(14, 14, 4, 5));
+        
+        /*
+        Redimensionnement des lignes et colonnes par défaut
+        */
         sheet.setDefaultColumnWidth(15);
         sheet.setDefaultRowHeightInPoints(22);
         
@@ -46,22 +122,11 @@ public class Factures {
         DEBUT 1 LIGNE
         */
         
-        fonte = wb.createFont();
-        fonte.setFontHeightInPoints((short)18);
-        fonte.setFontName("Courier New");
-        fonte.setBold(true);
         cell = row.createCell(0);
         cell.setCellValue("Lemons Production");
-        cellStyle = wb.createCellStyle();
-        cellStyle.setFont(fonte);
-        cell.setCellStyle(cellStyle);
-        sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, 2)); // Fusionne les cellules 0;0 à 0;2
-        
+        cell.setCellStyle(cellStyleFont18Gras); 
         cell = row.createCell(5);
-        cellStyle = wb.createCellStyle();
-        cellStyle.setBorderRight(BorderStyle.MEDIUM);
-        cellStyle.setLeftBorderColor((short)8);
-        cell.setCellStyle(cellStyle);
+        cell.setCellStyle(cellStyleRight);
         
         /*
         FIN 1 LIGNE
@@ -70,29 +135,27 @@ public class Factures {
         /*
         DEBUT 2 LIGNE
         */
-        fonte = wb.createFont();
-        fonte.setFontHeightInPoints((short)16);
-        fonte.setFontName("Courier New");
         row = sheet.createRow(1);
-        cell = row.createCell(0);
-        //cell.setCellValue("Société de production audiovisuelle");
-        cellStyle = wb.createCellStyle();
-        cellStyle.setFont(fonte);
-        cellStyle.setBorderBottom(BorderStyle.MEDIUM);
-        cellStyle.setBottomBorderColor((short)8); // 8 = couleur noire
-        for (int i = 0; i <= 5; i++) {
+        for (int i = 0; i <= 5; ++i) {
             cell = row.createCell(i);
-            cell.setCellStyle(cellStyle);
             if (i == 0) {
                 cell.setCellValue("Société de production audiovisuelle");
-            }   
+                cellStyle = wb.createCellStyle();
+                cellStyle.setFont(fonte16);
+                cellStyle.setBorderBottom(BorderStyle.MEDIUM);
+                cellStyle.setBottomBorderColor((short)8);
+                cell.setCellStyle(cellStyle);
+            }
+            else if(i != 0){
+            cell.setCellStyle(cellStyleBottom);
+            }
         }
-        sheet.addMergedRegion(new CellRangeAddress(1,1,0,3));
         cell = row.createCell(5);
         cellStyle = wb.createCellStyle();
         cellStyle.setBorderBottom(BorderStyle.MEDIUM);
+        cellStyle.setBottomBorderColor((short)8);
         cellStyle.setBorderRight(BorderStyle.MEDIUM);
-        cellStyle.setLeftBorderColor((short)8);
+        cellStyle.setRightBorderColor((short)8);
         cell.setCellStyle(cellStyle);
         
         /*
@@ -102,17 +165,11 @@ public class Factures {
         /*
         DEBUT 3 LIGNE
         */
-        
-        fonte = wb.createFont();
-        fonte.setFontHeightInPoints((short)12);
-        fonte.setFontName("Courier New");
+
         row = sheet.createRow(2);
         cell = row.createCell(0);
         cell.setCellValue("n° d'identification : 450 621 818 R.C.S");
-        cellStyle = wb.createCellStyle();
-        cellStyle.setFont(fonte);
-        cell.setCellStyle(cellStyle);
-        sheet.addMergedRegion(new CellRangeAddress(2, 2, 0, 2));
+        cell.setCellStyle(cellStyleFont12);
         
         /*
         FIN 3 LIGNE
@@ -122,16 +179,10 @@ public class Factures {
         DEBUT 4 LIGNE
         */
         
-        fonte = wb.createFont();
-        fonte.setFontHeightInPoints((short)13);
-        fonte.setFontName("Courier New");
         row = sheet.createRow(3);
         cell = row.createCell(0);
         cell.setCellValue("TVA intracommunautaire : FR 37 450 621 818 921 C");
-        cellStyle = wb.createCellStyle();
-        cellStyle.setFont(fonte);
-        cell.setCellStyle(cellStyle);
-        sheet.addMergedRegion(new CellRangeAddress(3, 3, 0, 3));
+        cell.setCellStyle(cellStyleFont12);
         
         /*
         FIN 4 LIGNE
@@ -141,16 +192,10 @@ public class Factures {
         DEBUT 5 LIGNE
         */
         
-        fonte = wb.createFont();
-        fonte.setFontHeightInPoints((short)13);
-        fonte.setFontName("Courier New");
         row = sheet.createRow(4);
         cell = row.createCell(0);
         cell.setCellValue("5, impasse Mousset");
-        cellStyle = wb.createCellStyle();
-        cellStyle.setFont(fonte);
-        cell.setCellStyle(cellStyle);
-        sheet.addMergedRegion(new CellRangeAddress(4, 4, 0, 1));
+        cell.setCellStyle(cellStyleFont12);
         
         /*
         FIN LIGNE 5
@@ -160,15 +205,10 @@ public class Factures {
         DEBUT LIGNE 6
         */
         
-        fonte = wb.createFont();
-        fonte.setFontHeightInPoints((short)13);
-        fonte.setFontName("Courier New");
         row = sheet.createRow(5);
         cell = row.createCell(0);
         cell.setCellValue("75012 Paris");
-        cellStyle = wb.createCellStyle();
-        cellStyle.setFont(fonte);
-        cell.setCellStyle(cellStyle);
+        cell.setCellStyle(cellStyleFont12);
         
         /*
         FIN LIGNE 6
@@ -178,25 +218,29 @@ public class Factures {
         DEBUT LIGNE 7
         */
         
-        fonte = wb.createFont();
-        fonte.setFontHeightInPoints((short)13);
-        fonte.setFontName("Courier New");
         row = sheet.createRow(6);
         cell = row.createCell(0);
         cell.setCellValue("Sébastien AUTRET");
-        cellStyle = wb.createCellStyle();
-        cellStyle.setFont(fonte);
-        cell.setCellStyle(cellStyle);
-        sheet.addMergedRegion(new CellRangeAddress(6, 6, 0, 1));
+        cell.setCellStyle(cellStyleFont12);
+        
         
         /*
         FIN LIGNE 7
         */
         
         /*
-        DEBUT LIGNE 8
+        DEBUT LIGNES 8 - 11
         */
         
+        HSSFRow row0 = sheet.createRow(7);
+        HSSFRow row1 = sheet.createRow(8);
+        HSSFRow row2 = sheet.createRow(9);
+        HSSFRow row3 = sheet.createRow(10);
+        HSSFRow[] lesLignes = new HSSFRow[4];
+        lesLignes[0] = row0;
+        lesLignes[1] = row1;
+        lesLignes[2] = row2;
+        lesLignes[3] = row3;
         for(int i = 4 ; i <= 5 ; i++){
             for(int j = 7 ; j <= 10 ; j++){
                 if(i == 4){
@@ -204,8 +248,7 @@ public class Factures {
                     fonte.setFontHeightInPoints((short)16);
                     fonte.setFontName("Courier New");
                     fonte.setBold(true);
-                    row = sheet.createRow(j);
-                    cell = row.createCell(i);
+                    cell = lesLignes[j - 7].createCell(i);
                     cellStyle = wb.createCellStyle();
                     cellStyle.setFont(fonte);
                     cellStyle.setBorderLeft(BorderStyle.MEDIUM);
@@ -216,15 +259,15 @@ public class Factures {
                         cell.setCellValue("FACTURE");
                         cellStyle.setBorderTop(BorderStyle.MEDIUM);
                         cellStyle.setTopBorderColor((short)8);
-                        sheet.addMergedRegion(new CellRangeAddress(7, 7, 4, 5));
+                        
                     }
                     else if(j == 8){
                         cell.setCellValue("Bird Office");
-                        sheet.addMergedRegion(new CellRangeAddress(8, 8, 4, 5));
+                        
                     }
                     else if(j == 9){
                         cell.setCellValue("29 Bid Lannes");
-                        sheet.addMergedRegion(new CellRangeAddress(9, 9, 4, 5));
+                        
                     }
                     else if(j == 10){
                         cell.setCellValue("75116 Paris");
@@ -232,12 +275,11 @@ public class Factures {
                         cellStyle.setBottomBorderColor((short)8);
                         cellStyle.setBorderRight(BorderStyle.MEDIUM);
                         cellStyle.setRightBorderColor((short)8);
-                        sheet.addMergedRegion(new CellRangeAddress(10, 10, 4, 5));
+                        
                     }
                 }
-                /*else if(i == 5){
-                    row = sheet.createRow(j);
-                    cell = row.createCell(i);
+                else if(i == 5){
+                    cell = lesLignes[j - 7].createCell(i);
                     cellStyle = wb.createCellStyle();
                     cellStyle.setBorderRight(BorderStyle.MEDIUM);
                     cellStyle.setRightBorderColor((short)8);
@@ -249,26 +291,113 @@ public class Factures {
                         cellStyle.setBorderBottom(BorderStyle.MEDIUM);
                         cellStyle.setBottomBorderColor((short)8);
                     }
-                }*/
+                }
                 cell.setCellStyle(cellStyle);
             }
         }
         
-        /*fonte = wb.createFont();
-        fonte.setFontHeightInPoints((short)13);
-        fonte.setFontName("Courier New");
-        row = sheet.createRow(4);
-        cell = row.createCell(4);
-        cell.setCellValue("Sébastien AUTRET");
-        cellStyle = wb.createCellStyle();
-        cellStyle.setFont(fonte);
-        cell.setCellStyle(cellStyle);
-        sheet.addMergedRegion(new CellRangeAddress(7, 7, 0, 1));*/
-        
         /*
-        FIN LIGNE 8
+        FIN LIGNES 8 - 11
         */
         
+        /*
+        DEBUT LIGNE 15
+        */
+        
+        row = sheet.createRow(14);
+        cell = row.createCell(4);
+        cell.setCellValue("Paris, 03/01/2017");
+        cell.setCellStyle(cellStyleFont16);
+        
+        /*
+        FIN LIGNE 15
+        */
+        
+        /*
+        DEBUT LIGNE 17
+        */
+        
+        row = sheet.createRow(16);
+        for(int i = 0 ; i <= 2 ; i++){
+            cell = row.createCell(i);
+            cellStyle = wb.createCellStyle();
+            cellStyle.setBorderTop(BorderStyle.MEDIUM_DASHED);
+            cellStyle.setTopBorderColor((short)8);
+            cellStyle.setBorderBottom(BorderStyle.MEDIUM_DASHED);
+            cellStyle.setBottomBorderColor((short)8);
+            if(i == 0){
+                fonte = wb.createFont();
+                fonte.setFontHeightInPoints((short)15);
+                fonte.setFontName("Courier New");
+                fonte.setBold(true);
+                cell.setCellValue("Facture");
+                cellStyle.setFont(fonte);
+            }
+            else if(i == 2){
+                fonte = wb.createFont();
+                fonte.setFontHeightInPoints((short)17);
+                fonte.setFontName("Courier New");
+                cell.setCellValue("003/2017");
+                cellStyle.setFont(fonte);
+                cellStyle.setBorderRight(BorderStyle.MEDIUM_DASHED);
+                cellStyle.setRightBorderColor((short)8);
+            }
+            cell.setCellStyle(cellStyle);
+        }
+        
+        /*
+        FIN LIGNE 17
+        */
+        
+        /*
+        DEBUT LIGNES 20 - 27
+        */
+        
+        HSSFRow row19 = sheet.createRow(19);
+        HSSFRow row20 = sheet.createRow(20);
+        HSSFRow row21 = sheet.createRow(21);
+        HSSFRow row22 = sheet.createRow(22);
+        HSSFRow row23 = sheet.createRow(23);
+        HSSFRow row24 = sheet.createRow(24);
+        HSSFRow row25 = sheet.createRow(25);
+        HSSFRow row26 = sheet.createRow(26);
+        HSSFRow[] lesRows = new HSSFRow[8];
+        lesRows[0] = row19;
+        lesRows[1] = row20;
+        lesRows[2] = row21;
+        lesRows[3] = row22;
+        lesRows[4] = row23;
+        lesRows[5] = row24;
+        lesRows[6] = row25;
+        lesRows[7] = row26;
+        
+        for(int i = 19 ; i <= 26 ; i++){
+            for(int j = 0 ; j <= 5 ; j++){
+                cell = lesRows[i - 19].createCell(j);
+                cellStyle = wb.createCellStyle();
+                if(i == 19){
+                    cellStyle.setBorderTop(BorderStyle.MEDIUM);
+                    cellStyle.setTopBorderColor((short)8);
+                }
+                if(i == 26){
+                    cellStyle.setBorderBottom(BorderStyle.MEDIUM);
+                    cellStyle.setBottomBorderColor((short)8);
+                }
+                if(j >= 3 && j <= 5){
+                    cellStyle.setBorderRight(BorderStyle.MEDIUM);
+                    cellStyle.setRightBorderColor((short)8);
+                }
+                if( i == 20 && j == 4 || i == 20 && j == 5){
+                    cellStyle.setBorderBottom(BorderStyle.MEDIUM);
+                    cellStyle.setBottomBorderColor((short)8);
+                }
+                cell.setCellStyle(cellStyle);
+            }
+        }
+        
+        /*
+        FIN LIGNES 20 - 27
+        */
         
         
         /*cell.setCellValue(10);
